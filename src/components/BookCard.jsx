632f132fc7d2937caafcf7703556e3296a3e366b
Ram Rohith Maringanti <ramrohith999@gmail.com>
2026-05-17 14:05:46 +0530

    added navbar

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function BookCard({ book }) {
+  return (
+    <div className="border p-4 rounded shadow">
+      <h2 className="text-xl font-bold">{book.title}</h2>
+
+      <p>{book.author}</p>
+
+      <p>{book.category}</p>
+
+      <Link
+        to={`/book/${book.id}`}
+        className="text-blue-500"
+      >
+        View Details
+      </Link>
+    </div>
+  );
+}
+
+export default BookCard;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-black text-white p-4 flex gap-5">
+      <Link to="/">Home</Link>
+      <Link to="/books">Browse Books</Link>
+      <Link to="/add-book">Add Book</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
