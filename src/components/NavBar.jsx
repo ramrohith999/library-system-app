bde4ca79439b21fcb4b9e94a8705b2efdf022abf
Ram Rohith Maringanti <ramrohith999@gmail.com>
2026-05-20 11:09:09 +0530

    added navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,18 @@ import { Link } from "react-router-dom";
 
 const Navbar=()=> {
   return (
-    <nav className="bg-black text-white p-4 flex gap-5">
+    <nav
+className="
+bg-blue-900
+text-amber-50
+p-4
+flex
+gap-6
+sticky
+top-0
+z-50
+shadow-lg
+"     >
       <Link to="/">Home</Link>
       <Link to="/books">Browse Books</Link>
       <Link to="/add-book">Add Book</Link>
